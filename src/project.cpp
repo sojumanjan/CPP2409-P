4c902039726f954305637095f12fc68cc5857185
sojumanjan <gkfaoddl@gmail.com>
2024-12-19 23:16:29 +0900

    week 5 - Add Zero-Point-Questions And Finish Project

--- src/project.cpp
+++ src/project.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 //대답 개수 (최대 3)
 const int answer_count = 2;
+const int end_day = 15;
 //관리해야 하는 포인트 4가지 (종교, 자산, 군사, 민심) 범위 : 0~10
 int religion_point = 5, property_point = 5, military_point = 5, citizen_point = 5;
 
@@ -47,15 +48,18 @@ int main(){
             if (condition == "normal"){
                 if (!general_question.GetQuestions(condition).empty()){
                     question = &general_question; //question을 일반 질문 객체로 설정
-                    question_number = rand() % question->GetQuestions(condition).size(); //랜덤 질문 선정
                 }
                 else{
-                    cout << "남아있는 일반 질문이 없습니다." << endl;
+                    cout << "남아있는 일반 질문이 없습니다. 게임을 종료합니다." << endl;
                     break;
                 }
             }
+            //포인트가 0일 때 특수 질문 하도록 설정
+            else if (CheckZeroPoint() == true){
+                question = &special_question;
+            }
             //1/4 확률로 특수 질문 객체로 설정
-            else{
+            else {
                 if (!special_question.GetQuestions(condition).empty()){
                     int random_int = rand() % 4;
                     if (random_int == 0) {
@@ -69,14 +73,14 @@ int main(){
                     cout << "남아있는 특수 질문이 없습니다." << endl;
                     question = &general_question;
                 }
-                question_number = rand() % question->GetQuestions(condition).size();
             }
-        
+            //랜덤 질문 인덱스 생성
+            question_number = rand() % question->GetQuestions(condition).size();
 
         //일 수 추가 후 종료조건 확인
         day_count++;
-        if (day_count == 20){
-            cout << "10일차가 되었습니다. 당신은 국왕으로서의 책무를 다했습니다. 축하합니다." << endl;
+        if (day_count == end_day){
+            cout << end_day << "일차가 되었습니다. 당신은 국왕으로서의 책무를 다했습니다. 축하합니다." << endl;
             break;
         }
 
@@ -98,22 +102,26 @@ int main(){
                 continue;
             }
         }
-        //답변에 따른 인덱스 설정 (답이 1이면 index를 0으로, 2면 index를 4로 설정)
-        default_index = (answer_number - 1) * 4;
-        //기본 인덱스 ~ +3까지 각 포인트에 증감. 최댓값은 10.
-        religion_point = min(religion_point + question->GetPointChange(question_number, default_index), 10);
-        property_point = min(property_point + question->GetPointChange(question_number, default_index+1), 10);
-        military_point = min(military_point + question->GetPointChange(question_number, default_index+2), 10);
-        citizen_point = min(citizen_point + question->GetPointChange(question_number, default_index+3), 10);
+        
         //답변에 대한 문구 출력
         cout << question->GetQuestions(condition)[question_number][answer_number + 2] << endl;
+        //포인트가 0이라면 특수 질문 바로 위에서 출력 후 break.
+        if (CheckZeroPoint() == true) break;
+        //답변에 따른 인덱스 설정 (답이 1이면 index를 0으로, 2면 index를 4로 설정)
+        default_index = (answer_number - 1) * 4;
+        //기본 인덱스 ~ +3까지 각 포인트에 증감. 최댓값은 10, 최솟값은 0
+        religion_point = max(min(religion_point + question->GetPointChange(question_number, default_index), 10), 0);
+        property_point = max(min(property_point + question->GetPointChange(question_number, default_index+1), 10), 0);
+        military_point = max(min(military_point + question->GetPointChange(question_number, default_index+2), 10), 0);
+        citizen_point = max(min(citizen_point + question->GetPointChange(question_number, default_index+3), 10), 0);
+        
         //포인트 변화량 문구 출력
         ChangePointPrint(question_number, default_index, question);
-        //포인트 0인지 확인, 맞다면 문구 출력 후 break
-        if (CheckZeroPoint() == true) break;
-        
+        //사용했던 질문 삭제
         question->DeleteQuestion(question_number);
     }
+    cout << "최종 스코어입니다." << endl;
+    DrawUI(day_count);
     cout << "게임을 종료합니다." << endl;
 }
 
@@ -158,33 +166,31 @@ void PointDown(int question_number, int index, string point_name, Question* ques
     cout << point_name << "포인트가 " << -1 * ques->GetPointChange(question_number, index) <<"만큼 감소했습니다." << endl;
 }
 bool CheckZeroPoint(){
-    if (religion_point <= 0){
-        cout << "종교 포인트가 0이 되었습니다." << endl << "당신은 종교인들의 반란을 막지 못하고 십자가에 걸려 죽음을 맞이했습니다." << endl;
-        return true;
-    }
-    if (property_point <= 0){
-        cout << "자산 포인트가 0이 되었습니다." << endl << "소공왕국의 국고는 결국 바닥이 나서 파산했습니다." << endl;
-        return true;
-    }
-    if (military_point <= 0){
-        cout << "군사 포인트가 0이 되었습니다." << endl << "소공왕국의 병사 수가 급격하게 감소했습니다. 이에 옆 왕국에서 들어온 침략을 막지 못하고 멸망했습니다." << endl;
-        return true;
-    }
-    if (citizen_point <= 0){
-        cout << "민심 포인트가 0이 되었습니다." << endl << "모든 백성들이 당신에게 단단히 화가 났습니다. 반기를 들고 왕국에 쳐들어와 당신은 죽음을 맞이했습니다." << endl;
+    if (religion_point <= 0 || property_point <= 0 || military_point <= 0 || citizen_point <= 0){
         return true;
     }
     return false;
 }
 
 string ConditionCheck(){
-    if (religion_point >= 9) return "r+";
-    else if (property_point >= 9) return "p+";
-    else if (military_point >= 9) return "m+";
-    else if (citizen_point >= 9) return "c+";
-    else if (religion_point <= 2) return "r-";
-    else if (property_point <= 2) return "p-";
-    else if (military_point <= 2) return "m-";
-    else if (citizen_point <= 2) return "c-";
+    vector<string> condition;
+    //특정 포인트가 0이 되었을 때 컨디션
+    if (religion_point <= 0) return "r--";
+    if (property_point <= 0) return "p--";
+    if (military_point <= 0) return "m--";
+    if (citizen_point <= 0) return "c--";
+    //특정 포인트가 2이하, 9이상일 때의 컨디션
+    if (religion_point >= 9) condition.push_back("r+");
+    if (property_point >= 9) condition.push_back("p+");
+    if (military_point >= 9) condition.push_back("m+");
+    if (citizen_point >= 9) condition.push_back("c+");
+    if (religion_point <= 2) condition.push_back("r-");
+    if (property_point <= 2) condition.push_back("p-");
+    if (military_point <= 2) condition.push_back("m-");
+    if (citizen_point <= 2) condition.push_back("c-");
+    //특수 상태라면,(중복가능) 랜덤하게 리턴.
+    if (condition.empty() == false){
+        return condition[rand() % condition.size()];
+    }
     else return "normal";
 }
